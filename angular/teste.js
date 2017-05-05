0b54534f98f972c4880a9313694e7f604cd06512
Ricardo Braga <ricardocjca@gmail.com>
2017-05-05 05:48:06 -0400

    commit testeCrudIndt 1

--- angular/teste.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('meuTeste', ['ngRoute', 'User']).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-	$routeProvider.when('/', {templateUrl: 'views/index.html'}).otherwise({redirectTo:'/'});
-}]);
