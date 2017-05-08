9eeaa22b466aebe425bf2b1da577e1ba3446b601
Ricardo Braga <ricardocjca@gmail.com>
2017-05-07 21:21:24 -0400

    atualizacao do projeto

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var config = {headers:  {
+        'Authorization': 'TokenEx',
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        "X-Login-Ajax-call": 'true'
+    }
+};
+
+var meuApp = angular.module("meuApp", ['angularUtils.directives.dirPagination']);
+
